bc09e561c2df00a569e31f1c6be7327f81d70729
hariabi2001 <61871536+hariabi2001@users.noreply.github.com>
2023-10-11 21:59:37 +0530

    Update section.cpp

--- Book-library/section.cpp
+++ Book-library/section.cpp
@@ -1,8 +1,7 @@
 #include "section.h"
 
 
-Section::Section(int pageNumber, std::shared_ptr<authorBookData> authors)
+Section::Section(int pageNumber, std::shared_ptr<authorBookData> authors): pageNumber(pageNumber), authors(authors)
 {
-    this->pageNumber = pageNumber;
-    this->authors = authors;
+    
 }
